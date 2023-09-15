a2f9998adcb8d6ebe52a92e53b36a0dbf2f7d04d
Minh Tri <minhtriphung@gmail.com>
2023-09-15 23:10:32 +0700

    - Show vote average

--- TrendingMovies/TrendingMovies/Presentation/TrendingMovies/Views/TrendingMovieCollectionViewCell.swift
+++ TrendingMovies/TrendingMovies/Presentation/TrendingMovies/Views/TrendingMovieCollectionViewCell.swift
@@ -14,6 +14,8 @@ class TrendingMovieCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var releaseDateLabel: UILabel!
     @IBOutlet weak var moviePosterImageView: UIImageView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var voteAverageLabel: UILabel!
+    @IBOutlet weak var voteAverageView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,11 +23,12 @@ class TrendingMovieCollectionViewCell: UICollectionViewCell {
         self.moviePosterImageView.layer.cornerRadius = 10.0
         self.activityIndicator.startAnimating()
         self.activityIndicator.hidesWhenStopped = true
+        self.voteAverageView.layer.cornerRadius = self.voteAverageView.frame.width / 2
     }
 
     func setupData(_ movie: TrendingMovie) {
         self.movieNameLabel.text = movie.title
-        
+        self.voteAverageLabel.text = String(format: "%.1f", movie.voteAverage)
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "yyyy-MM-dd"
